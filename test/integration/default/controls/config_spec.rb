ec3ce5d1cfcf7b6a32e988a1e129f3b19bccb33c
vveliev <vveliev@tucows.com>
2022-10-19 20:11:14 -0400

    fix: updating tests

--- test/integration/default/controls/config_spec.rb
+++ test/integration/default/controls/config_spec.rb
@@ -5,7 +5,7 @@ control 'Grafana configuration' do
 
   describe file('/etc/grafana/grafana.ini') do
     it { should be_file }
-    it { should be_owned_by 'root' }
+    it { should be_owned_by 'grafana' }
     it { should be_grouped_into 'grafana' }
     its('mode') { should cmp '0640' }
     its('content') { should include 'File managed by Salt' }
--- test/integration/default/controls/subcomponent_config.rb
+++ test/integration/default/controls/subcomponent_config.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
-control 'grafana.subcomponent.config.file' do
-  title 'Verify the subcomponent configuration file'
+# control 'grafana.subcomponent.config.file' do
+#   title 'Verify the subcomponent configuration file'
 
-  describe file('/etc/grafana-subcomponent-formula.conf') do
-    it { should be_file }
-    it { should be_owned_by 'root' }
-    it { should be_grouped_into 'root' }
-    its('mode') { should cmp '0644' }
-    its('content') do
-      should include(
-        '# File managed by Salt at '\
-        '<salt://grafana/subcomponent/config/files/default/'\
-        'subcomponent-example.tmpl.jinja>.'
-      )
-    end
-    its('content') do
-      should include(
-        'This is another subcomponent example file from SaltStack '\
-        'template-formula.'
-      )
-    end
-  end
-end
+#   describe file('/etc/grafana-subcomponent-formula.conf') do
+#     it { should be_file }
+#     it { should be_owned_by 'root' }
+#     it { should be_grouped_into 'root' }
+#     its('mode') { should cmp '0644' }
+#     its('content') do
+#       should include(
+#         '# File managed by Salt at '\
+#         '<salt://grafana/subcomponent/config/files/default/'\
+#         'subcomponent-example.tmpl.jinja>.'
+#       )
+#     end
+#     its('content') do
+#       should include(
+#         'This is another subcomponent example file from SaltStack '\
+#         'template-formula.'
+#       )
+#     end
+#   end
+# end
